7c913be342eb8096e43a64018f669c0636c8e991
jishna999 <95485984+jishna999@users.noreply.github.com>
2022-05-23 11:40:15 +0530

    Update drawShapes.java

--- CO5/drawShapes.java
+++ CO5/drawShapes.java
@@ -1,13 +1,20 @@
 import java.awt.*;
 import java.applet.*;
 /*
-<applet code="Shapes" width=400 height=400>
+<applet code="drawShapes" width=400 height=400>
 </applet>
 */
-public class Shapes extends Applet {
-  public void paint(Graphics g) {
-    g.drawLine(10, 10, 100, 100);
-    g.drawRect(100, 150, 100, 150);
-    g.drawOval(200, 200, 200, 200);
-  }
+public class drawShapes extends Applet
+{
+	public void init()
+	{
+ 		setForeground(Color.black);
+		setBackground(Color.yellow);
+	}
+ 	 public void paint(Graphics g) 
+	{
+   		g.drawLine(10, 10, 100, 100);
+   		g.drawRect(100, 150, 100, 150);
+    		g.drawOval(200, 200, 200, 200);
+ 	}
 }
